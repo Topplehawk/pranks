c217dc9474a5ef661a76753efa53a5a86b149df1
Topplehawk <104155019+Topplehawk@users.noreply.github.com>
2022-05-11 12:56:09 -0600

    Add files via upload

---
+++ turnofflocalhostblockprank.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+ECHO Would you like to turn off Localhost block?
+ECHO Enter Y for yes
+ECHO Enter N for no
+ECHO This is case senstive
+CHOICE /c YN /m "YES or No"
+timeout /T 5
+ECHO you chose to turn off Localhost block
+timeout /T 5 
+ECHO turning off Localhost block
+timeout /T 5
+ECHO Localhost block has been turned off
